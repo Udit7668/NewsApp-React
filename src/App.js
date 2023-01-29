9e702103265d3a05d9f6d073d11f576b95cdba1e
Udit Mishra <294uditmishra@gmail.com>
2023-01-29 13:24:39 +0530

    added navbar in your application

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
 
 export default class App extends Component {
   
-   name="Ramesh"
+  // name="Ramesh"
   render() {
     return (
       <div>
-        Hello my first class based component {this.name};
+       {/*  Hello my first class based component {this.name}; */}
+
+        <NavBar/>
       </div>
     )
   }
